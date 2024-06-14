ebd514ae6bf71e19dfe2111a8700edeed017bfb7
lucpv@xdp.vn <lucpv@xdp.vn>
2024-06-15 01:13:31 +0700

    update

--- user/src/main/java/com/cinema_management/user/configuration/SecurityConfig.java
+++ user/src/main/java/com/cinema_management/user/configuration/SecurityConfig.java
@@ -18,7 +18,6 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
